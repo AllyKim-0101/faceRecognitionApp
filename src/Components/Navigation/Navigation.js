49dfb642c3df36cf28473841d3efdce0da7dc2de
Ally Kim <57968966+AllyKim-0101@users.noreply.github.com>
2021-01-25 16:14:05 +1100

    color modification

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,19 +1,23 @@
 import React from 'react';
 
 const Navigation = ({ onRouteChange, isSignedIn }) => {
-
+    const title = <h1>Face Recognition App</h1>;
     if (isSignedIn) {
-        return (
+        return (<>
+            {title}
             <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
                 <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'> Sign Out</p>
             </nav>
+        </>
         )
     } else {
-        return (
+        return (<>
+            {title}
             <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
                 <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'> Sign In</p>
                 <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'> Register</p>
             </nav>
+        </>
         );
     }
 }
